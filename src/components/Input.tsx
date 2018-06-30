d301f6388994888b0fdbb8cf4230a440190c598d
theapache64 <theapache64@gmail.com>
2018-07-01 01:08:59 +0530

    Splash remains

---
+++ src/components/Input.tsx
@@ -0,0 +1,10 @@
+import { default as React, Component } from 'react';
+import { TextInput } from 'react-native';
+
+export class Input {
+  render() {
+    return (
+      <TextInput />
+    );
+  }
+}
--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,11 +1,29 @@
 import { default as React, Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
+import { StyleSheet } from '../components/StyleSheet';
+import { GameConfig } from '../GameConfig';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent:'center',
+    backgroundColor: GameConfig.color.pauseColor,
+  },
+});
 
 export class SplashScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>Splash</Text>
+      <View style={styles.container}>
+
+        <TextInput
+          style={styles.input}
+        />
+
+        <TextInput
+          style={styles.input}
+        />
+
       </View>
     );
   }
